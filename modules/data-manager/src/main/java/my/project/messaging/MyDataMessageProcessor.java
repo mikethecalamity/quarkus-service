7179988e64e480be9c73e1c218b192c48e9b1a64
MiketheCalamity <mikethecalamity@users.noreply.github.com>
2025-04-10 18:20:22 -0400

    Updates

--- modules/data-manager/src/main/java/my/project/messaging/MyDataMessageProcessor.java
+++ modules/data-manager/src/main/java/my/project/messaging/MyDataMessageProcessor.java
@@ -7,6 +7,8 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import io.smallrye.reactive.messaging.annotations.Merge;
 import lombok.RequiredArgsConstructor;
 import my.project.db.MyDataRepository;
 import my.project.dto.MyDataMessage;
@@ -22,6 +24,8 @@ public class MyDataMessageProcessor {
 
     private final MyDataRepository dataRepository;
 
+    @Merge
+    @Broadcast
     @Incoming(INCOMING1_CHANNEL)
     @Outgoing(OUTGOING_CHANNEL)
     public Multi<MyDataMessage> consumeSource1(final Multi<MyDataMessage> message) {
@@ -29,6 +33,8 @@ public class MyDataMessageProcessor {
                 m -> dataRepository.persistAndFindLatest(m.getId(), m.getTimestamp(), Source.SOURCE1, m.getData()));
     }
 
+    @Merge
+    @Broadcast
     @Incoming(INCOMING2_CHANNEL)
     @Outgoing(OUTGOING_CHANNEL)
     public Multi<MyDataMessage> consumeSource2(final Multi<MyDataMessage> message) {
