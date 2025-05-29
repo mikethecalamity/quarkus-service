6a56383db706c3a8c91a765ffd13363c7401093b
MiketheCalamity <mikethecalamity@users.noreply.github.com>
2025-05-29 18:04:33 -0400

    Test quarkus-artemis-jms

--- modules/data-manager/src/main/java/my/project/rest/MyDataResource.java
+++ modules/data-manager/src/main/java/my/project/rest/MyDataResource.java
@@ -1,52 +1,22 @@
 package my.project.rest;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
+import jakarta.jms.ConnectionFactory;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.jboss.resteasy.reactive.RestPath;
-import org.jboss.resteasy.reactive.RestStreamElementType;
-
-import io.quarkus.arc.profile.IfBuildProfile;
-import io.quarkus.runtime.Startup;
-import io.smallrye.mutiny.Multi;
-import my.project.dto.MyDataMessage;
-import my.project.messaging.MyDataMessageProcessor;
-import my.project.type.Source;
-
-@Startup
-@Path("data1")
-@IfBuildProfile("dev")
+@Path("data")
 public class MyDataResource {
 
     @Inject
-    @Channel(MyDataMessageProcessor.INCOMING1_CHANNEL)
-    Emitter<MyDataMessage> incomingSource1Channel;
+    ConnectionFactory connectionFactory;
 
-    @Inject
-    @Channel(MyDataMessageProcessor.INCOMING2_CHANNEL)
-    Emitter<MyDataMessage> incomingSource2Channel;
-
-    @Inject
-    @Channel(MyDataMessageProcessor.OUTGOING_CHANNEL)
-    Multi<MyDataMessage> outgoingChannel;
-
-    @POST
-    @Path("consume/{source}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @RestStreamElementType(MediaType.APPLICATION_JSON)
-    public Multi<MyDataMessage> consume(@RestPath final Source source, final MyDataMessage message) {
-        if (source == Source.SOURCE1) {
-            incomingSource1Channel.send(message);
-        }
-        else if (source == Source.SOURCE2) {
-            incomingSource2Channel.send(message);
-        }
-
-        return outgoingChannel;
+    @GET
+    @Path("test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "TEST";
     }
 }
